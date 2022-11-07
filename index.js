2813a3ac4cf1589da30d835acf635bae02307409
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-07 13:59:34 +0100

    Add dotenv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
+require("dotenv").config();
+
 const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 
@@ -19,20 +21,6 @@ const routesDir = fs.readdirSync(routesPath);
 routesDir.forEach((route) => {
     require(path.join(routesPath, route))(app);
 });
-
-// Routes
-// require("./routes/categories/get")(app);
-// require("./routes/categories/post")(app);
-// require("./routes/categories/put")(app);
-// require("./routes/categories/delete")(app);
-// require("./routes/posts/get")(app);
-// require("./routes/posts/post")(app);
-// require("./routes/posts/put")(app);
-// require("./routes/posts/delete")(app);
-// require("./routes/users/get")(app);
-// require("./routes/users/post")(app);
-// require("./routes/users/put")(app);
-// require("./routes/users/delete")(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
